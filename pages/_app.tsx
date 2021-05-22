4cb7007eb43d922dd581a37a5908c592ee43961c
Daniel Martins <martinsda@gmail.com>
2021-05-21 21:56:14 -0300

    Added connection to injected wallet provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import { AppProps } from 'next/app';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 
 import '../styles/globals.css'
 
+const getLibrary = (provider: any): Web3Provider => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
+import { useWeb3React, UnsupportedChainIdError, Web3ReactProvider } from '@web3-react/core';
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
+import { Web3Provider } from '@ethersproject/providers';
+import { formatEther } from '@ethersproject/units';
+
+import { injected } from '../connectors';
+import { useEagerConnect, useInactiveListener } from '../hooks';
 import styles from '../styles/Home.module.css'
 
+enum ConnectorNames {
+  Injected = 'Injected'
+}
+
+const connectorsByName: { [connectorName in ConnectorNames]: any } = {
+  [ConnectorNames.Injected]: injected
+}
+
+const getLibrary = (provider: any): Web3Provider => {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
+
 export default function Home() {
+  const context = useWeb3React<Web3Provider>();
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context;
+
+  // handle logic to recognize the connector currently being activated
+  const [activatingConnector, setActivatingConnector] = useState<any>();
+  useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
+  }, [activatingConnector, connector]);
+
+  // handle logic to eagerly connect to the ethereum provider, if it exists and has granted access already
+  const triedEager = useEagerConnect();
+
+  // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+  useInactiveListener(!triedEager || !!activatingConnector);
+
+  // const { active, error } = useWeb3React()
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +56,62 @@ export default function Home() {
       </Head>
 
       <h1>Let's buidl this DeFi application</h1>
+
+      <div
+        style={{
+          display: 'grid',
+          gridGap: '1rem',
+          gridTemplateColumns: '1fr 1fr',
+          maxWidth: '20rem',
+          margin: 'auto'
+        }}
+      >
+        {Object.keys(connectorsByName).map(name => {
+          const currentConnector = connectorsByName[name]
+          const activating = currentConnector === activatingConnector
+          const connected = currentConnector === connector
+          const disabled = !triedEager || !!activatingConnector || connected || !!error
+
+          return (
+            <button
+              style={{
+                height: '3rem',
+                borderRadius: '1rem',
+                borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
+                cursor: disabled ? 'unset' : 'pointer',
+                position: 'relative'
+              }}
+              disabled={disabled}
+              key={name}
+              onClick={() => {
+                setActivatingConnector(currentConnector)
+                activate(connectorsByName[name])
+              }}
+            >
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '0',
+                  left: '0',
+                  height: '100%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  color: 'black',
+                  margin: '0 0 0 1rem'
+                }}
+              >
+                {/* {activating && <Spinner color={'black'} style={{ height: '25%', marginLeft: '-1rem' }} />} */}
+                {connected && (
+                  <span role="img" aria-label="check">
+                    ✅
+                  </span>
+                )}
+              </div>
+              {name}
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
